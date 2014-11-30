c66fd296c994ebbacbd27addb70b00d0f8ea72f9
Camille TJHOA <camille.tjhoa@gmail.com>
2014-11-30 17:06:45 +0100

    Fix interpolation

--- scripts/cpu_percentage.sh
+++ scripts/cpu_percentage.sh
@@ -6,7 +6,7 @@ source "$CURRENT_DIR/helpers.sh"
 
 print_cpu_percentage() {
 	if [ -e "/proc/stat" ]; then
-		grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+		grep 'cpu ' /proc/stat | awk '{usage=(\$2+\$4)*100/(\$2+\$4+\$5)} END {print usage "%"}'
 	fi
 }
 
