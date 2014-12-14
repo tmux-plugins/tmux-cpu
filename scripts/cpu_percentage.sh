145a3d2c2b9e22598e70b6313405bab640091bad
Larry Glenn <yrral.glenn@gmail.com>
2014-12-14 18:33:26 -0500

    Force number of devices to 1 so as not to break the awk script.
    The awk snippet that grabs the cpu stats assumes that there's one disk
    in iostat's output. Adding -n 1 to the iostat call ensures that iostat
    will only report on one disk, and the cpu stats will be in the correct
    column.

--- scripts/cpu_percentage.sh
+++ scripts/cpu_percentage.sh
@@ -8,7 +8,7 @@ print_cpu_percentage() {
 	if [ -e "/proc/stat" ]; then
 		grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf("%.1f%", usage)}'
 	elif [ is_osx ]; then
-		iostat -w 1 -c 2 | tail -1 | awk '{usage=100-$6} END {printf("%d%%",usage)}';
+		iostat -w 1 -c 2 -n 1 | tail -1 | awk '{usage=100-$6} END {printf("%d%%",usage)}';
 	fi
 }
 
