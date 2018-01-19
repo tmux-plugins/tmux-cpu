12f7a74e25bf59701456e2c0d98b39bb19ec7039
Camille TJHOA <camille.tjhoa@outlook.com>
2018-01-19 08:42:24 +0000

    Fix #21
    Remove empty lines which cause issue with some version of `iostat`. See #21 for more details.

--- scripts/cpu_percentage.sh
+++ scripts/cpu_percentage.sh
@@ -8,16 +8,16 @@ print_cpu_percentage() {
 	if command_exists "iostat"; then
 
 		if is_linux_iostat; then
-			iostat -c 1 2 | tail -n 2 | head -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 1 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
 		elif is_osx; then
-			iostat -c 2 disk0 | tail -n 1 | awk '{usage=100-$6} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 2 disk0 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$6} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
 		elif is_freebsd || is_openbsd; then
-			iostat -c 2 | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
 		else
 			echo "Unknown iostat version please create an issue"
 		fi
 	elif command_exists "sar"; then
-		sar -u 1 1 | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+		sar -u 1 1 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
 	else
 		if is_cygwin; then
 			usage="$(WMIC cpu get LoadPercentage | grep -Eo '^[0-9]+')"
