4b9267164a29bcd4a527da31ae15ede418ebb680
Casper da Costa-Luis <casper.dcl@physics.org>
2019-02-21 16:18:54 +0000

    remove extraneous spacing
    - uses %3.1f for all CPU stats
    - fixes #25

--- scripts/cpu_percentage.sh
+++ scripts/cpu_percentage.sh
@@ -8,24 +8,24 @@ print_cpu_percentage() {
 	if command_exists "iostat"; then
 
 		if is_linux_iostat; then
-			iostat -c 1 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 1 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%3.1f%%", usage)}' | sed 's/,/./'
 		elif is_osx; then
-			iostat -c 2 disk0 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$6} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 2 disk0 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$6} END {printf("%3.1f%%", usage)}' | sed 's/,/./'
 		elif is_freebsd || is_openbsd; then
-			iostat -c 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+			iostat -c 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%3.1f%%", usage)}' | sed 's/,/./'
 		else
 			echo "Unknown iostat version please create an issue"
 		fi
 	elif command_exists "sar"; then
-		sar -u 1 1 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
+		sar -u 1 1 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%3.1f%%", usage)}' | sed 's/,/./'
 	else
 		if is_cygwin; then
 			usage="$(WMIC cpu get LoadPercentage | grep -Eo '^[0-9]+')"
-			printf "%5.1f%%" $usage
+			printf "%3.1f%%" $usage
 		else
 			load=`ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {print s}'`
 			cpus=$(cpus_number)
-			echo "$load $cpus" | awk '{printf "%5.2f%%", $1/$2}'
+			echo "$load $cpus" | awk '{printf "%3.1f%%", $1/$2}'
 		fi
 	fi
 }
