beaf4573ab866261d612dac85fdf95d7da10dd4d
钟声 <nicky_zs@163.com>
2020-03-01 21:58:29 +0800

    update color names

--- scripts/cpu_fg_color.sh
+++ scripts/cpu_fg_color.sh
@@ -8,9 +8,9 @@ cpu_low_fg_color=""
 cpu_medium_fg_color=""
 cpu_high_fg_color=""
 
-cpu_low_default_fg_color="#[fg=green]"
-cpu_medium_default_fg_color="#[fg=yellow]"
-cpu_high_default_fg_color="#[fg=red]"
+cpu_low_default_fg_color="#[fg=colour10]"
+cpu_medium_default_fg_color="#[fg=colour3]"
+cpu_high_default_fg_color="#[fg=colour1]"
 
 get_fg_color_settings() {
   cpu_low_fg_color=$(get_tmux_option "@cpu_low_fg_color" "$cpu_low_default_fg_color")
