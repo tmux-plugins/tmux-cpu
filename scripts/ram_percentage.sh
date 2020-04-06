9a0ab23597ab40be07d58a7ec0914523a832f6e9
mushroom <50598611+ms-jpq@users.noreply.github.com>
2020-04-05 20:04:00 -0700

    Update ram_percentage.sh

--- scripts/ram_percentage.sh
+++ scripts/ram_percentage.sh
@@ -16,7 +16,7 @@ print_ram_percentage() {
   ram_percentage_format=$(get_tmux_option "@ram_percentage_format" "$ram_percentage_format")
 
   if command_exists "free"; then
-    free | awk -v format="$ram_percentage_format" '$1 ~ /Mem/ {printf(format, 100*$3/$2)}'
+    LC_ALL=C free | awk -v format="$ram_percentage_format" '$1 ~ /Mem/ {printf(format, 100*$3/$2)}'
   elif command_exists "vm_stat"; then
     # page size of 4096 bytes
     stats="$(vm_stat)"
