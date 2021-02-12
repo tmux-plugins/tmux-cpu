88ba2431893fe0b105cfc9d3fef9b759ddece22e
Casper da Costa-Luis <casper.dcl@physics.org>
2021-02-12 16:14:41 +0000

    cpu_temp: support AMD Ryzen
    - fixes #66

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -11,7 +11,7 @@ print_cpu_temp() {
   cpu_temp_format=$(get_tmux_option "@cpu_temp_format" "$cpu_temp_format")
   cpu_temp_unit=$(get_tmux_option "@cpu_temp_unit" "$cpu_temp_unit")
   if command_exists "sensors"; then
-    ([ "$cpu_temp_unit" == F ] && sensors -f || sensors) | awk -v format="$cpu_temp_format$cpu_temp_unit" '/^Core [0-9]+/ {gsub("[^0-9.]", "", $3); sum+=$3; n+=1} END {printf(format, sum/n)}'
+    ([ "$cpu_temp_unit" == F ] && sensors -f || sensors) | sed -e 's/^Tccd/Core /' | awk -v format="$cpu_temp_format$cpu_temp_unit" '/^Core [0-9]+/ {gsub("[^0-9.]", "", $3); sum+=$3; n+=1} END {printf(format, sum/n)}'
   fi
 }
 
